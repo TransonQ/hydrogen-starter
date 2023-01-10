ca30c89b9d806117d2d1a90844eb6832dd38ffe5
quanscheng <quanscheng@163.com>
2023-01-10 21:57:58 +0800

    feat: layout内部异步数据优化加载Suspense

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,8 +1,12 @@
 import {
   CacheLong,
   gql,
-  Link, Seo, useShopQuery, useUrl
+  Link,
+  Seo,
+  useShopQuery,
+  useUrl
 } from '@shopify/hydrogen'
+import { Suspense } from 'react'
 
 const SHOP_QUERY = gql`
   query ShopInfo {
@@ -27,46 +31,48 @@ export const Layout = ({ children }) => {
 
   return (
     <>
-      <Seo
-        type='defaultSeo'
-        data={{
-          title: shop.name,
-          description: shop.description,
-        }}
-      />
-      <div className='flex flex-col min-h-screen antialiased bg-neutral-50'>
-        <div className=''>
-          <a
-            href='#mainContent'
-            className='sr-only'
-          >
-            Skip to content
-          </a>
-        </div>
-        <header
-          role='banner'
-          className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm ${
-            isHome ? 'bg-black/80 text-white' : 'bg-white/80'
-          }`}
-        >
-          <div className='flex gap-12'>
-            <Link
-              className='font-bold'
-              to='/'
+      <Suspense fallback={<>Layout inside loading...</>}>
+        <Seo
+          type='defaultSeo'
+          data={{
+            title: shop.name,
+            description: shop.description,
+          }}
+        />
+        <div className='flex flex-col min-h-screen antialiased bg-neutral-50'>
+          <div className=''>
+            <a
+              href='#mainContent'
+              className='sr-only'
             >
-              {shop.name}
-            </Link>
+              Skip to content
+            </a>
           </div>
-        </header>
+          <header
+            role='banner'
+            className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm ${
+              isHome ? 'bg-black/80 text-white' : 'bg-white/80'
+            }`}
+          >
+            <div className='flex gap-12'>
+              <Link
+                className='font-bold'
+                to='/'
+              >
+                {shop.name}
+              </Link>
+            </div>
+          </header>
 
-        <main
-          role='main'
-          id='mainContent'
-          className='flex-grow'
-        >
-          {children}
-        </main>
-      </div>
+          <main
+            role='main'
+            id='mainContent'
+            className='flex-grow'
+          >
+            {children}
+          </main>
+        </div>
+      </Suspense>
     </>
   )
 }
